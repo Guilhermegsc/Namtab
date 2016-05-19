05345b2a26d0dac2b70bb48a3061d73ce91377d1
gustavo.soliveir16 <gustavo.soliveir16@CAS419174W0706.senacsp.edu.br>
2016-05-18 21:24:11 -0300

    Redirecionar para mesma pagina no Login

---
+++ src/main/java/ErroLoginServlet.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import static com.sun.corba.se.spi.presentation.rmi.StubAdapter.request;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import javax.ws.rs.core.Response;
+
+/**
+ *
+ * @author gustavo.soliveir16
+ */
+@WebServlet(urlPatterns = {"/ErroServlet"})
+public class ErroLoginServlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+     
+    }
+
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        
+      request.getRequestDispatcher("WEB-INF/login.jspx").forward(request, response);
+        
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+        request.getRequestDispatcher("WEB-INF/login.jspx").forward(request, response);
+    }
+
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
