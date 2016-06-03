a6d81fbd9ecaa1b043136c9174cc90d02b53da68
Thiago <Thiago@10.0.2.15>
2016-06-03 13:18:50 -0300

    Campo codigo aceitando somente numeros TELA EXCLUIR VENDA.

--- src/main/webapp/js/alteraVenda.js
+++ src/main/webapp/js/alteraVenda.js
@@ -1,7 +1,19 @@
 window.addEventListener("load", teste)
-function teste(){
+function teste() {
     x = document.getElementById("mensagem").value;
-    if(x != ""){
+    if (x != "") {
         alert(x);
+    }
+}
+
+function somenteNumero(e) {
+    var tecla = (window.event) ? event.keyCode : e.which;
+    if ((tecla > 47 && tecla < 58 || tecla == 13))
+        return true;
+    else {
+        if (tecla == 8 || tecla == 0)
+            return true;
+        else
+            return false;
     }
 }
