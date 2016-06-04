2eb3d89c1a90829e630fc1819bf302ab28c97027
guilherme.gsilv10 <guilherme.gsilv10@CAS485725W07B10.senacsp.edu.br>
2016-06-04 12:59:00 -0300

    CRUD de Administração pronto. Falta apenas validações.

--- src/main/webapp/js/administracao.js
+++ src/main/webapp/js/administracao.js
@@ -1,8 +1,20 @@
 window.addEventListener("load", iniciar);
 function iniciar() {
     var cpf = document.getElementById("cpf").value; 
-    if(cpf !== 0){
+    if(cpf !== ""){
             document.getElementById("cpf").disabled = true;
+            document.getElementById("senha").style.display = "none";
+            document.getElementById("cnfSenha").style.display = "none";
+            document.getElementById("lblSenha").style.display = "none";
+            document.getElementById("lblConfirma").style.display = "none";
+    }
+    else{
+                    document.getElementById("cpf").disabled = false;
+            document.getElementById("senha").style.display = "inline";
+            document.getElementById("cnfSenha").style.display = "inline";
+            document.getElementById("lblSenha").style.display = "inline";
+            document.getElementById("lblConfirma").style.display = "inline";
+        
     }
 }
 function pesquisar(){
